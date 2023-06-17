3c0155336d3ee744bce21ebd520ef335c909a448
Thiago <thiagogobb@outlook.com>
2023-06-17 14:25:45 -0300

    chore adding test images + fixing pragma

--- TDEParallelAlgorithm.c
+++ TDEParallelAlgorithm.c
@@ -6,169 +6,214 @@
 #define MAX_WIDTH 100
 #define MAX_HEIGHT 100
 
+int main()
+{
+    double sumOfTimes = 0;
+    for (int i = 0; i < 10000; i++)
+    {
 
-int main() {
+        char filename[] = "./images/test3.pbm"; // Replace with your PBM file path
 
-    double ti, tf, tempo;
-    ti = tf = tempo = 0;
-    struct timeval tempo_inicio, tempo_fim;
-    gettimeofday(&tempo_inicio, NULL);   
-
-    char filename[] = "test2.pbm";  // Replace with your PBM file path
-
-    FILE *file = fopen(filename, "r");
-    if (file == NULL) {
-        printf("Failed to open the file.\n");
-        return 1;
-    }
-
-    // Read PBM file header
-    char magicNumber[3];
-    int width, height;
-
-    fscanf(file, "%s", magicNumber);
-    if (strcmp(magicNumber, "P1") != 0) {
-        printf("Invalid PBM file format.\n");
-        fclose(file);
-        return 1;
-    }
-
-    fscanf(file, "%d %d", &width, &height);
-    if (width > MAX_WIDTH || height > MAX_HEIGHT) {
-        printf("Image dimensions exceed the maximum array size.\n");
-        fclose(file);
-        return 1;
-    }
-
-    // Read pixel data
-    int i, j;
-    int image[MAX_HEIGHT][MAX_WIDTH];
-
-    for (i = 0; i < height; i++) {
-        for (j = 0; j < width; j++) {
-            fscanf(file, "%d", &image[i][j]);
+        FILE *file = fopen(filename, "r");
+        if (file == NULL)
+        {
+            printf("Failed to open the file.\n");
+            return 1;
         }
-    }
 
-    fclose(file);
+        // Read PBM file header
+        char magicNumber[3];
+        int width, height;
 
-    // Print the 2D array
-    printf("Image dimensions: %d x %d\n", width, height);
-
-//--------------------------------------------------------------------------------------------
-
-    //INICIANDO TRANSFORMAÇÃO SAITO TORIWAKI
-
-    int T1[height][width];
-    for(int i=0; i<height; i++){
-        for(int j=0; j<width; j++){
-            T1[i][j] = 0;
+        if (fscanf(file, "%2s", magicNumber) != 1)
+        {
+            printf("Failed to read magic number.\n");
+            fclose(file);
+            return 1;
         }
-    }
 
-    // Perform the first transformation
-    #pragma opm parallel for
-    for (int i = 0; i < height; i++) {
-        for (int j = 0; j < width; j++) {
-            if (image[i][j] == 0) {
-                for (int k = 0; k < width; k++) {
-                    if (image[i][k] == 1) {
-                        int distanceSquared = (i - i) * (i - i) + (j - k) * (j - k);
+        if (strcmp(magicNumber, "P1") != 0)
+        {
+            printf("Invalid PBM file format.\n");
+            fclose(file);
+            return 1;
+        }
 
-                        if (distanceSquared < T1[i][j] || T1[i][j] == 0) {
-                            T1[i][j] = distanceSquared;
+        if (fscanf(file, "%d %d", &width, &height) != 2)
+        {
+            printf("Failed to read image dimensions.\n");
+            fclose(file);
+            return 1;
+        }
+
+
+        if (width > MAX_WIDTH || height > MAX_HEIGHT)
+        {
+            printf("Image dimensions exceed the maximum array size.\n");
+            fclose(file);
+            return 1;
+        }
+
+        // Read pixel data
+        int i, j;
+        int image[MAX_HEIGHT][MAX_WIDTH];
+
+        for (i = 0; i < height; i++)
+        {
+            for (j = 0; j < width; j++)
+            {
+                if (fscanf(file, "%d", &image[i][j]) != 1)
+                {
+                    printf("Failed to read pixel data.\n");
+                    fclose(file);
+                    return 1;
+                }
+            }
+        }
+
+        fclose(file);
+
+        // Print the 2D array
+        // printf("Image dimensions: %d x %d\n", width, height);
+
+//--------------------------------Starting Saito and Toriwaki algorithm and setting timer------------------------------------------------------------
+
+
+
+        double ti, tf, tempo;
+        ti = tf = tempo = 0;
+        struct timeval tempo_inicio, tempo_fim;
+        gettimeofday(&tempo_inicio, NULL);
+
+        int T1[height][width];
+        for (int i = 0; i < height; i++)
+        {
+            for (int j = 0; j < width; j++)
+            {
+                T1[i][j] = 0;
+            }
+        }
+
+//----------------------------------Perform the first transformation----------------------------------------------
+#pragma omp parallel for
+        for (int i = 0; i < height; i++)
+        {
+            for (int j = 0; j < width; j++)
+            {
+                if (image[i][j] == 0)
+                {
+                    for (int k = 0; k < width; k++)
+                    {
+                        if (image[i][k] == 1)
+                        {
+                            int distanceSquared = (i - i) * (i - i) + (j - k) * (j - k);
+
+                            if (distanceSquared < T1[i][j] || T1[i][j] == 0)
+                            {
+                                T1[i][j] = distanceSquared;
+                            }
                         }
                     }
                 }
             }
         }
-    }
 
-// -----------------------------------------------------------------------------------------
+//--------------------------------Perform the second transformation---------------------------------
 
-    //Perform the second transformation
-    int TDE[height][width];
 
-    for(int i=0; i<height; i++){
-        for(int j=0; j<width; j++){
-            TDE[i][j] = 0;
-        }
-    }
+        int TDE[height][width];
 
-    // Perform the second transformation
-    #pragma opm parallel for
-    for (int j = 0; j < width; j++) {
-
-        for (int i = 0; i < height; i++) {
-            int sum = 0; 
-            for (int k = 0; k < height; k++) {
-
-                int p = T1[k][j];
-
-                if(T1[k][j] == 0 && image[k][j] != 1){
-                    p = width*width;
-                }
-                
-                int distanceSquared = ((i - k) * (i - k)) + p;
-
-                if (distanceSquared < sum || sum == 0) {
-                    sum = distanceSquared;
-                }
-            }
-            if(image[i][j] == 1)
-                TDE[i][j] = 0;
-            else if(image[i][j] == 0){
-                TDE[i][j] = sum;
-            }
-            
-        }
-    }
-
-// -----------------------------------------------------------------------------------------
-    
-    printf("Image data:\n");
-
-    for (i = 0; i < height; i++) {
-        for (j = 0; j < width; j++) {
-            printf("%d ", image[i][j]);
-        }
-        printf("\n");
-    }
-
-    
-    printf("\n\n");
-    printf("First Transform data\n");
-
-    for (i = 0; i < height; i++)
-    {
-        for (j = 0; j < width; j++)
+        for (int i = 0; i < height; i++)
         {
-            printf("%d ", T1[i][j]);
+            for (int j = 0; j < width; j++)
+            {
+                TDE[i][j] = 0;
+            }
         }
-        printf("\n");
-    }
-    
 
-   
-    printf("\n\n");
-    printf("TDE Final data\n");
 
-    for (i = 0; i < height; i++)
-    {
-        for (j = 0; j < width; j++){
-            printf("%d ", TDE[i][j]);
+#pragma omp parallel for
+        for (int j = 0; j < width; j++)
+        {
+
+            for (int i = 0; i < height; i++)
+            {
+                int sum = 0;
+                for (int k = 0; k < height; k++)
+                {
+
+                    int p = T1[k][j];
+
+                    if (T1[k][j] == 0 && image[k][j] != 1)
+                    {
+                        p = width * width;
+                    }
+
+                    int distanceSquared = ((i - k) * (i - k)) + p;
+
+                    if (distanceSquared < sum || sum == 0)
+                    {
+                        sum = distanceSquared;
+                    }
+                }
+                if (image[i][j] == 1)
+                    TDE[i][j] = 0;
+                else if (image[i][j] == 0)
+                {
+                    TDE[i][j] = sum;
+                }
+            }
         }
-        printf("\n");
+
+// -------------------------------------------------------Print Data------------------------------
+        /*
+        printf("Image data:\n");
+
+        for (i = 0; i < height; i++) {
+            for (j = 0; j < width; j++) {
+                printf("%d ", image[i][j]);
+            }
+            printf("\n");
+        }
+
+
+        printf("\n\n");
+        printf("First Transform data\n");
+
+        for (i = 0; i < height; i++)
+        {
+            for (j = 0; j < width; j++)
+            {
+                printf("%d ", T1[i][j]);
+            }
+            printf("\n");
+        }
+
+
+
+        printf("\n\n");
+        printf("TDE Final data\n");
+
+        for (i = 0; i < height; i++)
+        {
+            for (j = 0; j < width; j++){
+                printf("%d ", TDE[i][j]);
+            }
+            printf("\n");
+        }
+        */
+
+// ------------------------------------End Timer--------------------------------------------------
+
+        gettimeofday(&tempo_fim, NULL);
+        tf = (double)tempo_fim.tv_usec + ((double)tempo_fim.tv_sec * (1000000.0));
+        ti = (double)tempo_inicio.tv_usec + ((double)tempo_inicio.tv_sec * (1000000.0));
+        tempo = (tf - ti) / 1000;
+        // printf("\n\nTempo de execução: %.3f ms\n", tempo);
+        sumOfTimes += tempo;
     }
-    
 
-// -----------------------------------------------------------------------------------------
-
-    gettimeofday(&tempo_fim, NULL);
-    tf = (double) tempo_fim.tv_usec + ((double) tempo_fim.tv_sec * (1000000.0));
-    ti = (double) tempo_inicio.tv_usec + ((double) tempo_inicio.tv_sec * (1000000.0));
-    tempo = (tf - ti) / 1000;
-    printf("\n\nTempo de execução: %.3f ms\n", tempo);
-
+// ------------------------------------Print Mean Time--------------------------------------------
+    printf("\n\nMean time of execution: %.3f ms\n", sumOfTimes / 10000);
     return 0;
 }
