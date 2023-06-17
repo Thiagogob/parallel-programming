e4aace094c8e12e471dfb26e55f66f8b50e2a101
Thiago <thiagogobb@outlook.com>
2023-06-17 19:20:26 -0300

    chore started analysis

--- analysis/analysis.py
+++ analysis/analysis.py
@@ -1,5 +1,32 @@
+import matplotlib
+import numpy as np
+matplotlib.use('agg') 
+
+import matplotlib.pyplot as plt
 def main():
-    print("Hello, World!")
+
+    with open("../timesSequential.txt") as file:
+        lines = file.readlines()
+
+    timesSequential = []
+
+    for line in lines:
+        time = float(line.strip())
+        timesSequential.append(time)
+    
+    mean = np.mean(timesSequential)
+
+    plt.plot(timesSequential, label = 'Time for each run')
+
+    plt.axhline(y=mean, color='r', linestyle='--', label = 'Mean time') 
+
+    plt.xlabel("Run")
+    plt.ylabel("Time (s)")
+    plt.title("Time for each Run and Mean Time")
+
+
+    plt.savefig('mean_plot.png', format='png', dpi=300)
+
 
 if __name__ == "__main__":
     main()
